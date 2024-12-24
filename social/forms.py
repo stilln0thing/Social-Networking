cd1d177a80722267b2be92b49e95fb0c014bad64
stilln0thing <atharv.2023ug3019@iiitranchi.ac.in>
2024-12-24 14:41:33 +0530

    Added post feature and social

---
+++ social/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    body = forms.CharField(
+        label='',
+        widget=forms.Textarea(attrs={
+            'rows' : '3',
+            'placeholder': 'Write your post here...'})
+    )
+    class Meta:
+        model = Post
+        fields = ['body']
--- socialnetwork/urls.py
+++ socialnetwork/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing.urls')),
     path('accounts/', include('allauth.urls')),
+    path('social/', include('social.urls')),
 ]
