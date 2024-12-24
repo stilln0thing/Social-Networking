00a389aff3aac3a4d177a67d421fc65d7f277184
stilln0thing <atharv.2023ug3019@iiitranchi.ac.in>
2024-12-24 17:07:49 +0530

    Added post and comment delete and edit feature

--- social/urls.py
+++ social/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView, PostEditView, PostDeleteView, CommentDeleteView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('post/edit/<int:pk>', PostEditView.as_view(), name='post_edit'),
+    path('post/delete/<int:pk>', PostDeleteView.as_view(), name='post_delete'),
+    path('post/<int:post_pk>/comment/delete/<int:pk>/', CommentDeleteView.as_view(), name='comment_delete')
 ]
--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -162,6 +162,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-LOGIN_REDIRECT_URL = 'index'
+LOGIN_REDIRECT_URL = 'post_list'
 ACCOUNT_EMAIL_REQUIRED = True
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
